0a7c94f9db271a5973514b167a919c98d0aadd55
Lance Fallon <fallon.lance@gmail.com>
2015-03-07 09:36:02 -0500

    add email to contacts table

--- src/main/java/com/lance/flywaydemo/models/Contact.java
+++ src/main/java/com/lance/flywaydemo/models/Contact.java
@@ -13,6 +13,8 @@ public class Contact {
 	private String name;
 	@Column("phone_number")
 	private String phoneNumber;
+	@Column("email")
+	private String email;	
 
 	public Contact() {
 	}
@@ -20,10 +22,12 @@ public class Contact {
 	@TemplateConstructor
 	public Contact(@PropertyName("id") Integer id,
 			@PropertyName("name") String name,
-			@PropertyName("phoneNumber") String phoneNumber) {
+			@PropertyName("phoneNumber") String phoneNumber,
+			@PropertyName("email") String email) {
 		this.id = id;
 		this.name = name;
 		this.phoneNumber = phoneNumber;
+		this.email = email;
 	}
 
 	public Integer getId() {
@@ -48,6 +52,14 @@ public class Contact {
 
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 }
--- src/main/webapp/WEB-INF/views/details.jsp
+++ src/main/webapp/WEB-INF/views/details.jsp
@@ -2,6 +2,7 @@
 	<div class="col-md-6">
 		<h3>{{contact.name}}</h3>
 		<h3>{{contact.phoneNumber}}</h3>
+		<h3>{{contact.email}}</h3>
 		<a class="btn btn-default" href="#/">Home</a>
 	</div>
 </div>
